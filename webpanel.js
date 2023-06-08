4d196d3b5882bbdfe4ec96c71a496bfaeea4bc7d
Jaydenha09 <98650905+Jaydenha09@users.noreply.github.com>
2023-06-08 15:16:24 +0800

    Update webpanel.js

--- webpanel.js
+++ webpanel.js
@@ -1,46 +1,7 @@
-const webpanel = require('./webpanel.js');
-const Discord = require('discord.js-selfbot-v13');
+const express = require('express');
+const app = express();
+const port = 3000;
 
-//connect mysql database
-mysql = require('mysql2');
+app.get('/', (req, res) => res.send('Hello World!'));
 
-var connection = mysql.createConnection({
-    host: 'HOST',
-    port: 'PORT',
-    user: 'USER',
-    password: 'PASSWORD'
-});
-
-connection.connect(function(err){
-    if(err) throw err;
-    console.log('database connected succeed!');
-});
-//end
-
-//discord auto respone 
-const client = new Discord.Client({
-  checkUpdate: false,
-  syncStatus: false
-});
-const token = process.env['token']
-const replyMessage = process.env['replyMessage']
-
-let isEnabled = true
-
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
-
-client.on('messageCreate', msg => {
-  if (isEnabled) {
-    if (msg.author.bot) return;
-    if (msg.mentions.has(client.user.id)) {
-      msg.reply(`${replyMessage}`);
-    }
-  }
-
-});
-
-client.login(token);
-
-//end of discord auto respond code
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
